ca5591afef0a6795784edde543876e875675a6cf
rldn12134 <dyddns101213@naver.com>
2026-03-10 18:42:50 +0900

    chore: 환경변수 이름 일부 수정

--- app/core/config.py
+++ app/core/config.py
@@ -3,10 +3,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Database(BaseModel):
-    url: MySQLDsn = Field(alias="DATABASE_URL")
-    pool_size: int = Field(10, alias="DB_POOL_SIZE")
-    max_overflow: int = Field(20, alias="DB_MAX_OVERFLOW")
-    pool_timeout: int = Field(30, alias="DB_POOL_TIMEOUT")
+    url: MySQLDsn = Field(alias="URL")
+    pool_size: int = Field(10, alias="POOL_SIZE")
+    max_overflow: int = Field(20, alias="MAX_OVERFLOW")
+    pool_timeout: int = Field(30, alias="POOL_TIMEOUT")
 
 
 class Settings(BaseSettings):
