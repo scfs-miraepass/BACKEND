ec30bfa057d3f1d3051fd45430ddb836bc128603
rldn12134 <dyddns101213@naver.com>
2026-06-22 02:38:06 +0000

    fix(database): 게시글 관련 스키마 메타데이터에 등록되지 않는 현상 해결
    [skip ci]

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,4 +1,15 @@
 from .users import User, Users, UserType, UserSearch
 from .point import PointHistory, PointHistoryType
+from .post import Post, Posts, PostContent
 
-__all__ = ["User", "Users", "UserType", "UserSearch", "PointHistory", "PointHistoryType"]
+__all__ = [
+    "User",
+    "Users",
+    "UserType",
+    "UserSearch",
+    "PointHistory",
+    "PointHistoryType",
+    "Post",
+    "Posts",
+    "PostContent",
+]
