f37e139daa7ca793409851ddf91c40bace657eef
rldn12134 <rldn12134@rldn.xyz>
2026-07-12 00:03:19 +0900

    feat: Client에서 유저를 가지고 올때, cache를 사용하는 옵션 추가

--- app/core/client.py
+++ app/core/client.py
@@ -12,13 +12,18 @@ class ServiceClient(BaseCore):
             cls.instance = super().__new__(cls)
         return cls.instance
 
-    async def get_user(self, _id: int) -> User | None:
+    async def get_user(self, _id: int, *, cache: bool = False) -> User | None:
         """
         ID를 이용해 사용자를 가져옵니다.
 
         Returns:
             User | None
         """
+        if cache:
+            cached_user = await self.redis.get(f"user:{_id}")
+            if cached_user:
+                return User(payload=Users.model_validate(cached_user))
+
         async with self.session as session:
             payload = await session.get(Users, _id)
         return User(payload=payload)
