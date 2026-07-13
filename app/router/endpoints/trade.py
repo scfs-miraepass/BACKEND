fd0bd38edb8a5c8fefba1e464ec36c948aa4adcb
rldn12134 <dyddns101213@naver.com>
2026-07-14 00:46:14 +0900

    feat: 거래 전체 목록 API

--- app/router/endpoints/trade.py
+++ app/router/endpoints/trade.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException, status
 from pydantic import BaseModel
+from sqlmodel import select, col
 
 from app.schemas import PointHistoryType, Trades
-from app.schemas.response import ErrorResponse
+from app.schemas.response import ErrorResponse, ResponseModel
 from app.core import ServiceClient, LoginDep
 
 router = APIRouter(
@@ -10,6 +11,7 @@ router = APIRouter(
     tags=["trade"],
 )
 client = ServiceClient()
+CACHE_KEY_LIST_ALL = "CACHE_KEY_LIST_ALL"
 
 
 class RequestBody(BaseModel):
@@ -28,10 +30,27 @@ class RequestBody(BaseModel):
         },
     },
     status_code=status.HTTP_200_OK,
+    response_model=ResponseModel[list[Trades]],
     summary="전체 거래 목록",
     description="모든 유저의 거래 목록을 가져옵니다.",
 )
-async def list_all_trades(auth_data: LoginDep): ...
+async def list_all_trades(auth_data: LoginDep):
+    user, _ = auth_data
+    if user.type != user.type.service:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Permission denied.",
+        )
+
+    async with client.session as session:
+        query = select(Trades).order_by(col(Trades.id).desc())
+        result = await session.execute(query)
+        trades = result.scalars().all()
+
+    json_data = [item.model_dump() for item in trades]
+    await client.redis.set(CACHE_KEY_LIST_ALL, json_data, ttl=60 * 60 * 24)  # 하루 캐싱
+
+    return ResponseModel[list[Trades]](success=True, data=trades)
 
 
 @router.post(
@@ -51,7 +70,8 @@ async def list_all_trades(auth_data: LoginDep): ...
     summary="거래 승인",
     description="거래를 승인합니다.",
 )
-async def approval(trade_id: int, auth_data: LoginDep): ...
+async def approval(trade_id: int, auth_data: LoginDep):
+    await client.redis.delete(CACHE_KEY_LIST_ALL)
 
 
 @router.post(
@@ -71,7 +91,8 @@ async def approval(trade_id: int, auth_data: LoginDep): ...
     summary="거래 거부",
     description="거래를 거부합니다.",
 )
-async def refusal(trade_id: int, auth_data: LoginDep): ...
+async def refusal(trade_id: int, auth_data: LoginDep):
+    await client.redis.delete(CACHE_KEY_LIST_ALL)
 
 
 @router.post(
@@ -119,6 +140,7 @@ async def request(item: RequestBody, auth_data: LoginDep):
                 reason=item.reason,
             )
         )
+        await client.redis.delete(CACHE_KEY_LIST_ALL)
         client.logs.service.info(
             f"{user.id}({user.name})가 {buyer_user.id}({buyer_user.name})과 거래를 신청했습니다. ({item.amount}. {item.reason[:10] + '...' if len(item.reason) > 10 else item.reason})"
         )
@@ -127,8 +149,20 @@ async def request(item: RequestBody, auth_data: LoginDep):
 @router.get(
     "",
     responses={200: {"description": "정상 처리"}},
+    response_model=ResponseModel[list[Trades]],
     status_code=status.HTTP_200_OK,
     summary="거래 목록",
     description="자기자신의 거래 목록을 가져옵니다.",
 )
-async def list_trade(auth_data: LoginDep): ...
+async def list_trade(auth_data: LoginDep):
+    user, _ = auth_data
+
+    async with client.session as session:
+        query = (
+            select(Trades)
+            .where((Trades.seller_id == user.id) | (Trades.buyer_id == user.id))
+            .order_by(col(Trades.id).desc())
+        )
+        result = await session.execute(query)
+        trades = result.scalars().all()
+    return ResponseModel[list[Trades]](success=True, data=trades)
