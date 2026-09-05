24b7f45913b26aec7bdc050c301195f41e79b7a1
rldn12134 <rldn12134@rldn.xyz>
2026-09-05 17:05:00 +0900

    feat: 예약 경매 입찰(KaraokeBid) 데이터 스키마 추가

--- app/schemas/karaoke.py
+++ app/schemas/karaoke.py
@@ -1,6 +1,9 @@
 from datetime import date, datetime
 from enum import StrEnum
-from sqlmodel import SQLModel, Field, Index, Column, String
+from pydantic import field_serializer
+from sqlmodel import SQLModel, Field, Index, Column, String, DateTime, Relationship, func
+
+from .core import SchemaCore
 
 
 class KaraokeStatus(StrEnum):
@@ -26,3 +29,33 @@ class Karaoke(SQLModel, table=True):
     end_time: datetime = Field(description="경매 종료 시간")
 
     min_point: int = Field(default=0, description="최소 입찰가")
+
+    bids: list["KaraokeBid"] = Relationship(back_populates="auction", passive_deletes=True)
+
+
+class KaraokeBid(SQLModel, table=True):
+    __tablename__ = "karaoke_bid"
+
+    id: int | None = Field(None, primary_key=True, index=True)
+
+    auction: Karaoke = Relationship(back_populates="bids")
+    auction_id: int = Field(foreign_key="karaoke.id", nullable=False, index=True, ondelete="CASCADE")
+
+    amount: int = Field(nullable=False, description="입찰 금액")
+
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(SchemaCore.timezone),
+        sa_column=Column(
+            DateTime(timezone=True),
+            server_default=func.now(),
+        ),
+    )
+
+    # TODO: 누가 입찰했는지 추가해야함, 이때 입찰자는 여러명일 수 있음으로 파티로 따로 묶어서 하자
+    # TODO: 경매의 최고 입찰가는 Redis에 저장하자
+
+    @field_serializer("created_at")
+    def serialize_created_at(self, dt, _info):
+        if isinstance(dt, datetime):
+            return SchemaCore.sync_timezone(dt).isoformat()
+        return dt
