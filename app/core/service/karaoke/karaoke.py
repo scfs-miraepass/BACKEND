305a7cec2266762fe862e9520eb107d29a734afc
rldn12134 <rldn12134@rldn.xyz>
2026-09-08 16:37:41 +0900

    save: 입찰하던거 커밋

--- app/core/service/karaoke/karaoke.py
+++ app/core/service/karaoke/karaoke.py
@@ -1,9 +1,12 @@
 from typing import TYPE_CHECKING
 from sqlmodel import delete, col
 
-from app.schemas import Karaokes, KaraokeStatus
+from app.schemas import Karaokes, KaraokeStatus, KaraokeBids, PointHistoryType
 
 from ...core import ServiceCore
+from ..user import User
+from .bid import KaraokeBid
+from .party import KaraokeParty
 
 if TYPE_CHECKING:
     _Type = Karaokes
@@ -21,6 +24,12 @@ class Karaoke(ServiceCore[Karaokes], _Type):
 
     - key에 들어가는 date요소의 포멧팅은 datetime의 기본 포멧팅인 YYYY-MM-DD으로 할 것.
     """
+
+    @property
+    def time_format(self) -> str:
+        if self.time >= 8:
+            return "점심"
+        return f"{self.time}교시"
 
     async def delete(self):
         """
@@ -54,3 +63,61 @@ class Karaoke(ServiceCore[Karaokes], _Type):
         self.logs.service_karaoke.info(
             f"노래방 예약 상태 변경 - {self.id}({self.date} / {self.time})의 상태가 '{_status}'으로 변경되었습니다."
         )
+
+    async def get_highest(self) -> KaraokeBid | None:
+        """
+        현재 경매의 최고가를 가져옵니다.
+
+        Returns:
+            KaraokeBid | None
+        """
+        bid = await self.redis.get(f"karaoke:{self.id}:highest")
+        if bid is None:
+            return None
+        return KaraokeBid.model_validate(bid)
+
+    async def add_bid(self, bidder: User, amount: int, party: KaraokeParty | None = None) -> KaraokeBid:
+        """
+        노래방 경매에 입찰합니다.
+
+        Args:
+            bidder: 입찰하는 유저
+            amount: 입찰 금액
+            party: 입찰하는 유저의 파티 고유 ID (선택 사항)
+
+        Raises:
+            ValueError: 입찰 조건(상태, 금액 등)을 만족하지 않을 경우 발생합니다.
+
+        Returns:
+            KaraokeBid
+        """
+        if self.status != KaraokeStatus.IN_PROGRESS:
+            raise ValueError("경매가 진행 중인 상태가 아닙니다.")
+
+        highest = await self.get_highest()
+        if amount < (self.min_point if highest is None else highest.amount):
+            raise ValueError(f"입찰 금액은 최소 입찰가({self.min_point}) 이상이어야 합니다.")
+
+        async with self.session as session:
+            obj = KaraokeBids(
+                auction_id=self.id, bidder_id=bidder.id, party_id=party.id if party is not None else None, amount=amount
+            )
+            session.add(obj)
+            await session.flush()
+
+        await self.redis.delete(f"karaoke:{self.id}")
+        await self.redis.delete_pattern(f"karaoke_list:{self.date}")
+
+        await self.redis.set(f"karaoke:{self.id}:highest", obj.model_dump())  # 최고 입찰가 갱신
+        # TODO: 위에 이거 TTL 설정필요
+
+        await bidder.point_deduct(
+            amount,
+            reason="노래방 예약",
+            memo=f"{self.date} {self.time_format} 노래방 예약",
+            type=PointHistoryType.karaoke_bid,
+        )  # 포인트 차감
+
+        # TODO: 입찰시 기존 최고가 입찰자 포인트 환불
+
+        return KaraokeBid(payload=obj)
